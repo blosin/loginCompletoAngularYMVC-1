a48b4fab15503202d0bbfbec9d694e8d54c91299
Martin Navarro <ejemplo@example.com>
2020-11-06 19:33:28 -0300

    Commit inicial

---
+++ LoginCompleto/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Threading;
+using System.Web.Http;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    [System.Web.Mvc.AllowAnonymous]
+    [System.Web.Mvc.RoutePrefix("api/login")]
+    public class LoginController : ApiController
+    {
+        [System.Web.Mvc.HttpGet]
+        [System.Web.Mvc.Route("echoping")]
+        public IHttpActionResult EchoPing()
+        {
+            return Ok(true);
+        }
+
+        [System.Web.Mvc.HttpGet]
+        [System.Web.Mvc.Route("echouser")]
+        public IHttpActionResult EchoUser()
+        {
+            var identity = Thread.CurrentPrincipal.Identity;
+            return Ok($" IPrincipal-user: {identity.Name} - IsAuthenticated: {identity.IsAuthenticated}");
+        }
+
+        [System.Web.Mvc.HttpPost]
+        [System.Web.Mvc.Route("authenticate")]
+        public IHttpActionResult Authenticate(LoginRequest login)
+        {
+            if (login == null) 
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            bool isCredentialValid = (login.Password == "123456");
+            if (isCredentialValid)
+            {
+                var token = TokenGenerator.GenerateTokenJwt(login.Username);
+                return Ok(token);
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
